33ca49eca91ced42693564f62cfc23bcea6d8098
arnaud <arnaud-mary@live.fr>
2023-09-11 12:06:29 +0100

    Add Button component into App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Button from './components/Button/Button.jsx';
 /**
  * 
  * @param {*} props 
  * @returns 
  */
 const App=(props) => {
-    return <div className='App'>DEMAT BREIZH</div>
+    return (
+    <div className='App'>DEMAT BREIZH<hr/>
+    <Button/>
+    <Button/>
+    </div>
+    )
 };
 
 export default App;
