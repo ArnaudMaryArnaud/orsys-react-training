db95b2f66d1e181df9d8082c87351888c92aed67
arnaud <arnaud-mary@live.fr>
2023-09-11 15:04:19 +0100

    Add event handling coming from parent

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -10,7 +10,7 @@ const App=(props) => {
     return (
     <div className='App'>DEMAT BREIZH<hr/>
     <Button text="benjamin" bgcolor="tomato" color="yellow" style={{width: '254px', padding:'60px 80px'}} />
-    <Button text="button" bgcolor="blue"/>
+    <Button text="button" bgcolor='blue' onClick={(arg) => {console.log('button 2 activated', arg)}}/>
     </div>
     )
 };
